bf69b67a212f482ae893fdfdb61091dc0002e718
Андрей Щиголь <andryushka05@gmail.com>
2024-04-03 20:19:10 +0300

    main

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new { Name = "John", Age = 30 };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+        }
+    }
+}
